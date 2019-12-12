4a542386593ee467d9448fec136b0587db95dc3d
yqin43 <55920732+yqin43@users.noreply.github.com>
2019-12-11 19:54:03 -0600

    Update Person.java

--- Person.java
+++ Person.java
@@ -2,33 +2,38 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Person {
-	private Set<Person> pred = new HashSet<>();// predecessor list
-	private Set<Person> succ = new HashSet<>();// successor list
+//	private Set<Person> pred = new HashSet<>();// predecessor list
+//	private Set<Person> succ = new HashSet<>();// successor list
+	private Set<Person> neighbors = new HashSet<>();
 	private String name;// name of the Person
 	private boolean visited;
-	
+
 	public Person(String name) {
 		this.name = name;
 	}
-	
-	public Set<Person> pred() {
-		return this.pred;
+
+//	public Set<Person> pred() {
+//		return this.pred;
+//	}
+//
+//	public Set<Person> succ() {
+//		return this.succ;
+//	}
+
+	public Set<Person> neighbors() {
+		return this.neighbors;
 	}
-	
-	public Set<Person> succ() {
-		return this.succ;
-	}
-	
+		
 	public String name() {
 		return this.name;
 	}
-	
+
 	public void setVisited(boolean visited) {
-        this.visited = visited;
-    }
-	
+		this.visited = visited;
+	}
+
 	public boolean getVisited() {
-        return visited;
-    }
-	
+		return visited;
+	}
+
 }
