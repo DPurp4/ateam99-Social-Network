9d7e1e9c0f9971c0cfd7ec21294adee6616b0db3
yqin43 <55920732+yqin43@users.noreply.github.com>
2019-12-11 23:27:19 -0600

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,3 +1,27 @@
+//                  ALL STUDENTS COMPLETE THESE SECTIONS
+// Title:           Social Network Visualizer Program
+// Files:           Graph.java
+// Semester:        Autumn 2019
+//
+// ATeam members:	Devin DuBeau, LEC 002, ddubeau@wisc.edu, dubeau
+//					Mihir Arora, LEC 001, mrarora@wisc.edu, marora
+//					Xiaoyuan Liu, LEC 001, xliu798@wisc.edu, xiaoyuanl
+//					Yuehan Qin, LEC 001, yqin43@wisc.edu, yuehan
+//					Reid Chen, LEC 001, ychen878@wisc.edu, reid
+//
+//////////////////// STUDENTS WHO GET HELP FROM OTHER THAN THEIR PARTNER //////
+//                   must fully acknowledge and credit those sources of help.
+//                   Instructors and TAs do not have to be credited here,
+//                   but tutors, roommates, relatives, strangers, etc do.
+//
+// Persons:          Identify persons by name, relationship to you, and email.
+//                   Describe in detail the the ideas and help they provided.
+//
+// Online sources:   avoid web searches to solve your problems, but if you do
+//                   search, be sure to include Web URLs and description of
+//                   of any information you find.
+//////////////////////////// 80 columns wide //////////////////////////////////
+
 package application;
 
 import java.util.HashSet;
@@ -5,8 +29,7 @@ import java.util.Set;
 
 /**
  * Filename: Graph.java
- * Project: ateam3
- * Authors:
+ * Project: a3
  * 
  * Directed and unweighted graph implementation
  */
