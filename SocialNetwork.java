0645b469ce381c22daadd49288e4b2fee1245fa8
DPurp4 <56167835+DPurp4@users.noreply.github.com>
2019-12-11 22:54:42 -0600

    Add files via upload

--- SocialNetwork.java
+++ SocialNetwork.java
@@ -1,3 +1,4 @@
+
 package application;
 
 import java.io.File;
@@ -28,24 +29,43 @@ import java.util.Set;
  */
 public class SocialNetwork implements SocialNetworkADT {
 
-  private Graph graph;
-  private String centralUser;
-  
+  private Graph graph; // graph of all users in social network
+  private String centralUser; // central user (perspective from which network will be explored
+
+  /**
+   * Getter method for central user
+   * 
+   * @return the central user
+   */
   public String getCentralUser() {
     return centralUser;
   }
-  
-  
-  
+
+
+
+  /**
+   * Constructor
+   */
   public SocialNetwork() {
     graph = new Graph();
-    
+
   }
 
+  /**
+   * Gets all the people in this social network
+   * 
+   * @return a set containing all people in the network
+   */
   public Set<Person> getAllPeople() {
     return graph.getAllNodes();
   }
 
+  /**
+   * Adds a friendship between two existing users
+   * 
+   * Note: friendships are undirected, if user A is friends with user B, then user B is friends with
+   * user A
+   */
   @Override
   public boolean addFriends(String name1, String name2) {
     Person person1 = new Person(name1);
@@ -54,6 +74,9 @@ public class SocialNetwork implements SocialNetworkADT {
 
   }
 
+  /**
+   * Removes a friendship between two existing users
+   */
   @Override
   public boolean removeFriends(String name1, String name2) {
     Person person1 = new Person(name1);
@@ -61,41 +84,56 @@ public class SocialNetwork implements SocialNetworkADT {
     return this.graph.removeEdge(person1, person2);
   }
 
+  /**
+   * Adds a user to this social network
+   */
   @Override
   public boolean addUser(String name) {
     Person person = new Person(name);
     return this.graph.addNode(person);
   }
 
+  /**
+   * Removes a user from this social network
+   */
   @Override
   public boolean removeUser(String name) {
     Person person = new Person(name);
     return this.graph.removeNode(person);
   }
 
+  /**
+   * Returns all the friends of the user specified
+   */
   @Override
   public Set<Person> getFriends(String name) {
     Person person1 = new Person(name);
     return this.graph.getNeighbors(person1);
   }
 
+  /**
+   * Returns all the mutual friends between the two users specified
+   */
   @Override
   public Set<Person> getMutualFriends(String name1, String name2) {
     Set<Person> p1Friends = this.getFriends(name1);
     Set<Person> p2Friends = this.getFriends(name2);
     Set<Person> mutualFriends = new HashSet<>(p1Friends);
-    mutualFriends.retainAll(p2Friends);
+    mutualFriends.retainAll(p2Friends); // intersection between two sets
     return mutualFriends;
   }
 
+  /**
+   * Gets the shortest path between two users
+   */
   @Override
   public List<Person> getShortestPath(String name1, String name2) {
     LinkedList<Person> bfsList = new LinkedList<Person>();
     Queue<Person> queue = new LinkedList<>();
     Map<String, Person> prev = new HashMap<>();
-    Person p1 = graph.getNode(name1);
-    Person p2 = graph.getNode(name2);
-    Person current = p1;
+    Person p1 = graph.getNode(name1); // start vertex
+    Person p2 = graph.getNode(name2); // end vertex
+    Person current = p1; // current vertex
 
     queue.add(current);
     current.setVisited(true);
@@ -130,58 +168,67 @@ public class SocialNetwork implements SocialNetworkADT {
   }
 
 
-  void DFSUtil(Person p, Graph temp) { 
-    p.setVisited(true); 
-    temp.addNode(p);
-    for (Person neighbor : graph.getNeighbors(p)) { 
-        if(!neighbor.getVisited())
-          DFSUtil(neighbor, temp); 
-    } 
+  /**
+   * Depth first search recursive helper method for connectedComponents
+   * 
+   * @param p    - person being visited & added to temp
+   * @param temp - temporary graph containing connected components
+   */
+  void DFSUtil(Person p, Graph temp) {
+    p.setVisited(true); // mark user as visited
+    temp.addNode(p); // add user to temporary graph
+    for (Person neighbor : graph.getNeighbors(p)) { // for each friend the person has
+      if (!neighbor.getVisited()) // if the friend has not been visited yet
+        DFSUtil(neighbor, temp); // call recursive helper method
+    }
+  }
 
-} 
-    void connectedComponents(Graph temp, Set<Graph> cc) { 
-      
-     for (Person visited: graph.getAllNodes()) {
-       visited.setVisited(false);
-     }
-      
-    // Mark all the vertices as not visited 
-    for(Person person: graph.getAllNodes()) { 
-        if(!person.getVisited()) { 
-            // print all reachable vertices 
-            // from v 
-            DFSUtil(person, temp); 
-            cc.add(temp);
-            temp = new Graph();
-        } 
-    } 
-} 
-  
-  
-  
+  void connectedComponents(Graph temp, Set<Graph> cc) {
+    for (Person visited : graph.getAllNodes()) {
+      visited.setVisited(false); // initially mark all users as not visited
+    }
+    for (Person person : graph.getAllNodes()) { // for each person in the list of people
+      if (!person.getVisited()) { // if person has not been visited yet
+        DFSUtil(person, temp); // call recursive helper method
+        cc.add(temp); // add temp as one element of the "connected components" set
+        temp = new Graph(); // clear temp
+      }
+    }
+  }
 
+
+
+  /**
+   * Returns a set of graphs, each graph containing the connected components of this social network
+   */
   @Override
   public Set<Graph> getConnectedComponents() {
-
     Graph temp = new Graph();
     Set<Graph> cc = new HashSet<Graph>();
     connectedComponents(temp, cc);
-
     return cc;
   }
 
+  /**
+   * Reads from a text file and creates a graph network
+   */
   @Override
   public void loadFromFile(File file) {
     Scanner scnr = null;
     try {
       scnr = new Scanner(file);
+      boolean hasDefault = false;
       while (scnr.hasNextLine()) {
-        String line = scnr.nextLine();
+        String line = scnr.nextLine().trim();
         String[] wordList = line.split(" ");
 
         if (wordList.length <= 1)
           continue;
         if (wordList[0].equals("a")) {
+          if (!hasDefault) {
+            hasDefault = true;
+            centralUser = wordList[1];
+          }
           if (wordList.length == 2)
             this.addUser(wordList[1]);
           if (wordList.length == 3)
@@ -204,12 +251,20 @@ public class SocialNetwork implements SocialNetworkADT {
     }
   }
 
+  /**
+   * Sets central user
+   * 
+   * @param name
+   */
 
   public void setCentralUser(String name) {
     this.centralUser = name;
 
   }
 
+  /**
+   * Writes current graph network to a text file in a specific format.
+   */
   @Override
   public void saveToFile(File file) {
     try {
@@ -229,14 +284,11 @@ public class SocialNetwork implements SocialNetworkADT {
         writer.close();
       }
     } catch (FileNotFoundException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     }
 
   }
-  
-  public static void main(String[] args) {
- 
-  }
-  
+
 }
+
+
